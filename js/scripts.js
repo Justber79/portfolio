6df771e5ada9249387045ffc4d3a507adcc202fc
Justber79 <151619604+Justber79@users.noreply.github.com>
2024-01-31 13:57:48 +0100

    Added changes to JavaScript libraries and plugins

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 alert("Welcome on Bereza Design page");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+    } else {
+    x.className = 'navtoggle';
+    }
+    }
